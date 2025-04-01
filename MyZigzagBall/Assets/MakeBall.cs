c76381814f18992607b4155269d5211cf079fb98
mokwongame <ictplusud@gmail.com>
2025-04-01 16:04:46 +0900

    MyZigzagBall 프로젝트 시작

---
+++ MyZigzagBall/Assets/MakeBall.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MakeBall : MonoBehaviour
+{
+    // public 의미: Unity Editor에서 ball 변수에 접근할 수 있도록 공용화 선언
+    public GameObject ball; // 변수 선언: 데이터형 변수명;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "Shader 오류 수정"
+git commit -m "MyZigzagBall 프로젝트 시작"
 git push -u origin main
