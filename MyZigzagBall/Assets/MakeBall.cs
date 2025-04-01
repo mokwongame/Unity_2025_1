dcd04fcc40ee55b8f6fa3b4d0411160396e82e5b
mokwongame <ictplusud@gmail.com>
2025-04-01 16:51:21 +0900

    makeBall() 스크립트 추가

--- MyZigzagBall/Assets/MakeBall.cs
+++ MyZigzagBall/Assets/MakeBall.cs
@@ -8,12 +8,24 @@ public class MakeBall : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // C# Script가 실행되면 바로 공이 생성
+        //makeBall();
+        //Invoke("makeBall", 3.0f);
+        // 3초 뒤에 makeBall() 호출하고 5초 주기로 계속 makeBall() 호출
+        InvokeRepeating("makeBall", 3.0f, 5.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void makeBall()
+    {
+        // 인스턴스(instance, 예시) 만들기
+        //Instantiate(ball); // 프리팹의 위치에서 생성
+        Instantiate(ball, transform.position, transform.rotation); // ball이란 게임 오브젝트를 실행시켜 생성(인스턴스)하기
+        // empty game object에서 설정한 위치에서 생성
     }
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "MyZigzagBall 프로젝트 시작"
+git commit -m "makeBall() 스크립트 추가"
 git push -u origin main
