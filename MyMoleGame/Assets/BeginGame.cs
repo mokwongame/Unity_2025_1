2cce222035079fb2174a8fc3e2d1a374b106d8a5
mokwongame <ictplusud@gmail.com>
2025-04-29 16:29:17 +0900

    게임 시작 씬 추가

---
+++ MyMoleGame/Assets/BeginGame.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginGame : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Invoke("beginGame", 5.0f); // 5초 뒤에 beginGame이 이름인 함수를 호출(invoke)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void beginGame()
+    {
+        SceneManager.LoadScene("MoleScene"); // 새로운 씬을 로딩
+    }
+}
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "UI Text로 점수를 표시"
+git commit -m "게임 시작 씬 추가"
 git push -u origin main
