3baeb2a0b27e3a11878a50c80c4b0eb395c8d9de
mokwongame <ictplusud@gmail.com>
2025-04-29 15:43:44 +0900

    UI Text로 점수를 표시

--- MyMoleGame/Assets/GameManager.cs
+++ MyMoleGame/Assets/GameManager.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
-
+using TMPro; // TextMeshPro를 쓰기(using: C 언어의 #include) 위한 선언
 public class GameManager : MonoBehaviour
 {
     // 밸런싱 특성 변수
     public float axisSpeed = 15.0f;
     int score = 0;  // 총 득점
+    public TMP_Text textScore;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        updateScore();
     }
 
     // Update is called once per frame
@@ -19,6 +20,12 @@ public class GameManager : MonoBehaviour
     }
 
     public int getScore() { return score; }
-    public void incScore() { score++; } // 득점 증가
-    public void decScore() { score--; } // 득점 감소
+    public void incScore() { score++; updateScore(); } // 득점 증가
+    public void decScore() { score--; updateScore(); } // 득점 감소
+
+    public void updateScore()
+    {
+        // TMP_Text 갱신: text 멤버에 문자열 할당
+        textScore.text = $"Score: {score}";
+    }
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "두더지 게임에 득점 추가"
+git commit -m "UI Text로 점수를 표시"
 git push -u origin main
